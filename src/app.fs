03afd4e74ba54f19bd2084805c1c8604da914614
Tomas Petricek <tomas@tomasp.net>
2022-11-03 10:27:18 +0100

    Better UX, builtin length function

--- src/app.fs
+++ src/app.fs
@@ -1,4 +1,4 @@
-module App
+﻿module App
 open Tbd.Html
 
 type Primitive =
@@ -14,6 +14,8 @@ and Expr =
   | Tuple of Node list
   | Primitive of Primitive
   | Let of string * Node * Node
+  | Apply of Node * Node list
+  | Reference of string
 
 let node e = { Expression = e; Evaluated = None }
 
@@ -61,6 +63,7 @@ let ops =
 
 let rec tokenizeIdent acc iacc input = 
   match input with 
+  | ('_' as c)::input
   | PLetter c::input -> tokenizeIdent acc (c::iacc) input
   | _ -> tokenize (TIdent(toString (List.rev iacc))::acc) input
 
@@ -92,6 +95,9 @@ and parseExpr toks =
   match toks with 
   | TLBrack::toks -> parseExprList TSemicolon TRBrack List toks
   | TLParen::toks -> parseExprList TComma TRParen Tuple toks
+  | TIdent id::TLParen::toks -> parseExprList TComma TRParen (fun args -> 
+      Apply(node(Reference(id)), args)) toks
+  | TIdent id::toks -> node(Reference(id)), toks
   | TNumber n::toks -> node(Primitive(Number n)), toks
   | TLet::TIdent id::TEquals::toks -> 
       match parseExpr toks with 
@@ -105,7 +111,7 @@ let sample = """
   let data = [
     (2013, 12.3);
     (2014, 56.7)
-  ] in 1
+  ] in builtin_length(data)
 """
 
 let toks = tokenize [] (List.ofSeq sample)
@@ -115,35 +121,22 @@ if toks' <> [] then failwithf "Expected end of expression but got: %A" toks
 
 let (|ExprShape|) e = 
   match e with
-  | List(nd) -> "list", nd, fun nd -> List nd
-  | Tuple(nd) -> "tuple", nd, fun nd -> Tuple nd
-  | Primitive(Number n) -> $"number({n})", [], fun _ -> e
-  | Primitive(String s) -> $"string({s})", [], fun _ -> e
-  | Let(id, n1, n2) -> $"let({id})", [n1;n2], fun nd -> 
-      Let(id, List.item 0 nd, List.item 1 nd)
+  | List(nd) -> "list", nd
+  | Tuple(nd) -> "tuple", nd
+  | Apply(id, nd) -> $"app(...)", id::nd
+  | Primitive(Number n) -> $"number({n})", []
+  | Primitive(String s) -> $"string({s})", []
+  | Let(id, n1, n2) -> $"let({id})", [n1;n2]
+  | Reference(id) -> $"ref({id})", []
 
-      (*
-let evaluateExpr e = 
-  printfn "Evaluating: %A" e
-  match e with 
-  | Primitive p -> Primitive p
-  //| List of Node list
-  //| Tuple of Node list
-  //| Let of string * Node * Node
-  | _ -> e
-
-let rec evaluate nd = 
-  match nd.Evaluated with 
-  | Some nde -> 
-      { nd with Evaluated = Some (evaluate nde) }
-  | None -> 
-      let (ExprShape(_, children, ctor)) = nd.Expression
-  if children |> List.forall (fun c -> c.Evaluated.IsSome) then 
-    { nd with Evaluated = Some (node (evaluateExpr nd.Expression)) }
-  else 
-    //let eval, unev = children |> List.partition (fun c -> c.Evaluated.IsSome)
-    nd //{ nd with Evaluated = Some (ctor (eval @ [ evaluate (List.head unev) ] @ List.tail unev))}
-    &*)
+let (|ExprEvalChildren|) e = 
+  match e with
+  | List(nd) -> nd, fun nd -> List nd
+  | Tuple(nd) -> nd, fun nd -> Tuple nd
+  | Apply(id, nd) -> id::nd, fun nds -> Apply(List.head nds, List.tail nds)
+  | Primitive _ | Reference _ -> [], fun _ -> e
+  | Let(id, nd, body) -> [nd], fun nds -> 
+      Let(id, List.head nds, body)
 
 let rec evalSite orig = 
   match orig.Evaluated with 
@@ -152,7 +145,7 @@ let rec evalSite orig =
       evalSite nd |> Option.map (fun (es, ctor) ->
         es, fun nnd -> { orig with Evaluated = Some (ctor nnd)})
   | None ->
-      let (ExprShape(_, children, rebuild)) = orig.Expression
+      let (ExprEvalChildren(children, rebuild)) = orig.Expression
       let rec childEvalSite skipped = function
         | c::cs -> 
             match evalSite c with 
@@ -164,59 +157,99 @@ let rec evalSite orig =
       | None -> Some(orig.Expression, fun nnd -> { orig with Evaluated = Some(nnd) })
       | Some es -> Some es
 
+let rec substitute v arg nd = 
+  if nd.Evaluated.IsSome then failwith "substitute: Cannot subsitutute in evaluated subexpressions"
+  match nd.Expression with 
+  | Apply(e, es) -> { nd with Expression = Apply(substitute v arg e, List.map (substitute v arg) es) }
+  | Let(id, e1, e2) -> { nd with Expression = Let(id, substitute v arg e1, substitute v arg e2) }
+  | Reference(id) when id = v -> arg
+  | Reference _ | List _ | Tuple _ | Primitive _ -> nd
+
+let rec mostEvalauted nd = 
+  match nd.Evaluated with 
+  | Some nd -> mostEvalauted nd
+  | _ -> nd
+
+let (|MostEvaluated|) nd = mostEvalauted nd
+
+let evalSingle e = 
+  match e with 
+  | List _ | Tuple _ | Primitive _ -> e
+  | Reference id when id.StartsWith("builtin") -> e
+  | Reference id -> failwith $"evalSingle: unbound variable {id}"
+  | Let(v, arg, body) -> (substitute v arg body).Expression
+  | Apply(MostEvaluated { Expression = Reference id }, args) when id.StartsWith "builtin" ->
+      let evaldArgs = List.map (fun a -> (mostEvalauted a).Expression) args
+      match id, evaldArgs with 
+      | "builtin_length", [List els] -> Primitive(Number(float els.Length))
+      | _ -> failwith $"evalSingle: Argument mismatch or unsupported operation '{id}'"
+  | Apply _ -> failwith "evalSingle: Unsupported application"
+
+
 let evaluate nd = 
   match evalSite nd with 
-  | Some(nd, f) -> 
-      //printfn "Eval: %A" nd
-      //printfn "Returns: %A" (f (node nd))
-      f (node nd)
+  | Some(nd, f) -> f (node (evalSingle nd))
   | None -> nd
 
 
 type Event = 
   | Toggle of string
   | Evaluate
+  | SelectStep of string * int
 
 type State = 
   { Display : Map<string, bool>
+    SelectedSteps : Map<string, int>
     Node : Node }
 
 let initial = 
   { Display = Map.empty
+    SelectedSteps = Map.empty 
     Node = e } 
 
-let update state evt = 
+let updateUnchecked state evt = 
   match evt with 
   | Toggle id -> 
       let v = Option.defaultValue true (Map.tryFind id state.Display)
       { state with Display = Map.add id (not v) state.Display }
+  | SelectStep(id, n) ->
+      { state with SelectedSteps = state.SelectedSteps.Add(id, n) }
   | Evaluate ->
       { state with Node = evaluate state.Node }
 
-let rec renderNode state trigger id nd = 
-  let (ExprShape(name, children, _)) = nd.Expression   
-  let table c c1 c2 = 
-    [ h?div [ "class"=>c ] [ 
-        h?div ["class"=>"ndexpr"] c1;
-        h?div ["class"=>"ndeval"] c2 ] ]
-  table (if nd.Evaluated.IsSome then "ndcontv" else "ndconth")
-    [ yield h?a [ 
-        "href" => "javascript:;"
-        "click" =!> fun _ _ -> trigger (Toggle id) ] [text name]
-      if nd.Evaluated.IsSome then 
-        yield text " (source)" 
-      elif children <> [] && Map.tryFind id state.Display = Some false then
-        yield text " (...)"
-      elif children <> [] then 
-        yield h?ul [] [
-          for i, c in Seq.indexed children do 
-            yield h?li [] (renderNode state trigger $"{id}{i}" c) ]
-    ] (
-      match nd.Evaluated with 
-      | None -> [ text "(unevaluated)" ]
-      | Some nd -> renderNode state trigger $"{id}e" nd
-    )
+let update state evt = 
+  try updateUnchecked state evt
+  with e -> printfn "ERROR: %s" e.Message; state
 
+let rec renderNode state trigger id nd = 
+  [ let steps = 
+      nd
+      |> Seq.unfold (fun st -> Option.map (fun x -> x, x) st.Evaluated) 
+      |> Seq.append [nd] |> Array.ofSeq
+    let selectedStep = state.SelectedSteps.TryFind id |> Option.defaultValue (steps.Length - 1)
+    let selectStep diff _ _ = trigger (SelectStep(id, selectedStep+diff))    
+    let (ExprShape(name, children)) = steps.[selectedStep].Expression
+    
+    if selectedStep > 0 then
+      yield h?a [ "href" => "javascript:;"; "click" =!> selectStep (-1) ] [ text " ◀"]
+    else yield text " ◀"
+    yield h?span [] [ text $" ({selectedStep+1}/{steps.Length})" ]
+    if selectedStep < steps.Length - 1 then
+      yield h?a [ "href" => "javascript:;"; "click" =!> selectStep (+1) ] [ text " ▶"]
+    else yield text " ▶"
+
+    yield text " "
+    yield h?a [ 
+      "href" => "javascript:;"
+      "click" =!> fun _ _ -> trigger (Toggle id) ] [ h?strong [] [ text name] ]
+
+    if children <> [] && Map.tryFind id state.Display = Some false then
+      yield text " (...)"
+    elif children <> [] then 
+      yield h?ul [] [
+        for i, c in Seq.indexed children do 
+          yield h?li [] (renderNode state trigger $"{id}{i}" c) ]
+  ]
 
 let render trigger state = 
   h?div [] [ 
